db3134bfc6745f6261976bbc0f6842611ad8fa2c
Korneliia <korneliiaPositive@gmail.com>
2023-10-31 20:10:23 +0100

    add responsive

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,5 +1,11 @@
 let nav = document.querySelector(".blockForLogoAndNav");
+let emptyDiv = document.querySelector(".emptyDiv");
 
 document.addEventListener("scroll", () => {
     nav.classList.add("fixedNav");
+    emptyDiv.style.display = "block";
+    if (window.scrollY === 0) {
+        nav.classList.toggle("fixedNav");
+        emptyDiv.style.display = "none";
+    }
 })
