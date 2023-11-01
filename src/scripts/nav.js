b02d157a4ada75dcfe46c6ffcc97c10908d0fd49
Korneliia <korneliiaPositive@gmail.com>
2023-11-01 14:17:35 +0100

    add responsive styles and images

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,6 +1,11 @@
 let nav = document.querySelector(".blockForLogoAndNav");
 let emptyDiv = document.querySelector(".emptyDiv");
 
+let burger = document.querySelector(".imgBurger");
+let blockForLogoAndBurger = document.querySelector(".blockForLogoAndBurger");
+let blockForLogoAndNav = document.querySelector(".blockForLogoAndNav");
+
+
 document.addEventListener("scroll", () => {
     nav.classList.add("fixedNav");
     emptyDiv.style.display = "block";
@@ -8,4 +13,12 @@ document.addEventListener("scroll", () => {
         nav.classList.toggle("fixedNav");
         emptyDiv.style.display = "none";
     }
+})
+
+burger.addEventListener("click", () => {
+    blockForLogoAndBurger.style.display = "none";
+    blockForLogoAndNav.style.display = "flex";
+    blockForLogoAndNav.classList.add("navBurger");
+    document.querySelector(".imageLogo").style.display = "none";
+    document.querySelector(".btnBookNow").style.display = "none";
 })
