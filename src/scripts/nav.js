6c62258b90666643ce0b5ef4f1d23b6dbb898a6e
Korneliia <korneliiaPositive@gmail.com>
2023-11-02 12:55:23 +0100

    add animation

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,24 +1,45 @@
+AOS.init();
+
+
 let nav = document.querySelector(".blockForLogoAndNav");
 let emptyDiv = document.querySelector(".emptyDiv");
 
 let burger = document.querySelector(".imgBurger");
 let blockForLogoAndBurger = document.querySelector(".blockForLogoAndBurger");
 let blockForLogoAndNav = document.querySelector(".blockForLogoAndNav");
-
-
+let navBurger = document.querySelector('.navBurger')
+let closeButton = document.querySelector('.closeButton')
+let aHrefInNav = document.querySelectorAll('.navBurger nav a')
 document.addEventListener("scroll", () => {
     nav.classList.add("fixedNav");
     emptyDiv.style.display = "block";
+    blockForLogoAndBurger.classList.add("fixedNavBurger")
     if (window.scrollY === 0) {
         nav.classList.toggle("fixedNav");
+        blockForLogoAndBurger.classList.toggle("fixedNavBurger")
         emptyDiv.style.display = "none";
     }
+
+
 })
 
 burger.addEventListener("click", () => {
-    blockForLogoAndBurger.style.display = "none";
-    blockForLogoAndNav.style.display = "flex";
-    blockForLogoAndNav.classList.add("navBurger");
-    document.querySelector(".imageLogo").style.display = "none";
-    document.querySelector(".btnBookNow").style.display = "none";
+    navBurger.style.top = '0px'
+    //blockForLogoAndBurger.style.display = "none";
+    //blockForLogoAndNav.style.display = "flex";
+    //  blockForLogoAndNav.classList.add("navBurger");
+    //  document.querySelector(".imageLogo").style.display = "none";
+    // document.querySelector(".btnBookNow").style.display = "none";
+})
+closeButton.addEventListener('click', () => {
+    navBurger.style.top = '-200px'
+})
+Array.from(aHrefInNav).forEach(element => {
+    console.log(2);
+    element.addEventListener('click', () => {
+        navBurger.style.top = '-200px'
+        console.log(1);
+
+    })
+
 })
